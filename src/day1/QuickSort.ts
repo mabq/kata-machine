c523ea0ad994263a6ac0fc8851dbf591430f0dbb
mabq <alejandro.banderas@me.com>
2025-02-01 20:41:27 -0500

    Quicksort unsignificant change

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,7 +1,7 @@
 // The test requires that we edit the array being passed as argument, so we
 // play with indices instead of creating new sub-arrays.
 
-function partition(arr: number[], start: number, end: number): number {
+function sort_section(arr: number[], start: number, end: number): number {
     // Does the sorting for the given section and returns the split index.
     // Take the last element as the pivot value.
     const pivot = arr[end];
@@ -24,7 +24,7 @@ function qs(arr: number[], start: number, end: number): void {
     if (start >= end) {
         return;
     }
-    const pivot_index = partition(arr, start, end);
+    const pivot_index = sort_section(arr, start, end);
     qs(arr, start, pivot_index - 1);
     qs(arr, pivot_index + 1, end);
 }
